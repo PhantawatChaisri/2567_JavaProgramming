71dbf945266be71f959771aecdfbfa5fe9ae6ecf
PhantawatChaisri <phantawatchaisri@gmail.com>
2024-12-04 11:23:36 +0700

    PracticeLab2

---
+++ Labsheet2/src/Lab202.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Lab202 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int number;
+		
+		System.out.print("Input number : ");
+		number = input.nextInt();
+		
+	}
+
+}
