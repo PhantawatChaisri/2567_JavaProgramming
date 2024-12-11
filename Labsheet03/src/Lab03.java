9f64d167fa45f6d033b57bef974920c87d6afa87
PhantawatChaisri <phantawatchaisri@gmail.com>
2024-12-11 11:21:20 +0700

    PracticeLab03

---
+++ Labsheet03/src/Lab03.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Lab03 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int specbar = 0, word = 0;
+		
+		System.out.println("Input the sentence : ");
+		String name = scan.nextLine();
+
+
+	}
+
+}
