6561e28a173fa0ece8aad89c0cadbeb05852134a
PhantawatChaisri <phantawatchaisri@gmail.com>
2024-12-25 10:30:21 +0700

    Homework04

--- Labsheet04/src/Lab404.java
+++ Labsheet04/src/Lab404.java
@@ -1,40 +1,58 @@
 import java.util.*;
-import java.util.concurrent.ThreadPoolExecutor.DiscardOldestPolicy;
 
 public class Lab404 {
-
 	public static void main(String[] args) {
-		inputStudent ();
+		inputstudent();
 	}
-	public static void inputStudent() {
-		Scanner scanner = new Scanner (System.in);
-		String studentID,subjectCode;
-		boolean isStudentID, isSubjectCode;
+	public static void inputstudent() {
+    Scanner input = new Scanner(System.in);
 		
-		while (true) {
+		String StudentID;
+		String SubjectID;
+		boolean isStudentID, isSubjectID;
+		
+		while(true) {
 			System.out.print("Enter student ID (10 digits): ");
-			studentID = scanner.nextLine();
+			StudentID = input.nextLine();
 			
-			System.out.print("Enter subject code (7 digits): ");
-			subjectCode = scanner.nextLine();
+			System.out.print("Enter Subject code (7 digits): ");
+			SubjectID = input.nextLine();
 			
-			isStudentID = isLenght(studentID, 10);
-			isSubjectCode = isLenght(subjectCode, 7);
+			isStudentID = isLenght(StudentID, 10);
+			isSubjectID = isLenght(SubjectID, 7);
 			
-			if (isStudentID && isSubjectCode) {
-				boolean isITStudent = isITstudent(studentID);
-				boolean isSubjectCode = isSubjectCode(subjectCode);
-				displayData(isITStudent,isSubjectCode,isStudentID);
+			if(isStudentID&&isSubjectID) {
+				boolean isITStudent = isITStudent(StudentID);
+				boolean isITSubject = isITSubject(SubjectID);
+				displayData(isITStudent,isITSubject,StudentID);
 				break;
-			}else {
-				System.out.print("");
+			} else {
+				System.out.println("");
 			}
 		}
 	}
-	public static boolean isLenght (String ID,int digits) {
+		
+	public static boolean isLenght(String ID,int digits) {
 		return ID.length()==digits;
 	}
+	public static boolean isITStudent(String ID) {
+		return ID.substring(3,6).equals("311")&&ID.charAt(2)=='1';		
+	}
+	public static boolean isITSubject(String ID) {
+		return ID.substring(0,2).equals("21")&&ID.charAt(4)=='1';		
+	}
+	public static void displayData(boolean stu, boolean sub, String ID) {
+		if(stu) {
+			System.out.println("Student id: " + ID +" 1st year student in IT");
+		}else {
+			System.out.println("Student id: " + ID +" is not 1st year student in IT");
+		}
+		if(sub) {
+			System.out.println("Enroll in courses for Year 1");
+		}else {
+			System.out.println("not enroll in courses for Year 1");
+		}
 		
 	}
-
+	
 }
