fb048967c247eb799d0fa0511ee65aa3b9e20b86
PhantawatChaisri <phantawatchaisri@gmail.com>
2025-01-15 11:20:37 +0700

    Practice3 don't have4,5

---
+++ Labsheet06/src/TestProduct.java
@@ -0,0 +1,14 @@
+public class TestProduct {
+
+	public static void main(String[] args) {
+		
+		Product product = new Product();
+
+		product.setProductDetails("Smartphone", 30000.0, 7.0);
+
+		System.out.println("Product Details:");
+		product.displayProductDetails();
+
+	}
+
+}
