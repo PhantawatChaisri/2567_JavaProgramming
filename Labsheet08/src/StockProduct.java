0f1e559fd6f312a3ce6db44f8c2bb411be50ec91
PhantawatChaisri <phantawatchaisri@gmail.com>
2025-02-05 11:19:05 +0700

    Homework08 have 3and5 add2

--- Labsheet08/src/StockProduct.java
+++ Labsheet08/src/StockProduct.java
@@ -7,14 +7,34 @@ public class StockProduct {
 		Scanner scan = new Scanner(System.in);
 		Products [] products = new Products [4];
 		
-		for (i = 0; i < products.length; i++ ) {
-			products[i] = new Products();
-			
-			System.out.println("Input product Id :");
-			products[i].setId(scan.nextLine());
+		for (int i =0;i<productList.length;i++) 
+		{
+			productList[i] = new Product();
+			System.out.print("Input product id : ");
+			String id = scan.nextLine();
+			productList[i].setId(id);
+			System.out.print("Input product Unit : ");
+			int unit = scan.nextInt();
+			scan.nextLine();
+			productList[i].setUnit(unit);
+			System.out.print("Input product Price : ");
+			double price = scan.nextDouble();
+			scan.nextLine();
+			productList[i].setPrice(price);
+			System.out.println();
 		}
 		
-
+		System.out.println("-----------------------------------------");
+		double totalPrice = 0;
+		for(Product product:productList) {
+			System.out.println("Product ID : "+product.getId()+"Total price = "+product.calculate()+" baht.");
+			totalPrice += product.calculate();
+		}
+		
+		System.out.println("-----------------------------------------");
+		System.out.println("Total price of all products is "+ totalPrice + " baht.");
+		scan.close();
+		
 	}
 
 }
