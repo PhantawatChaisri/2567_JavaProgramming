18d38ceafed4f621a2174e9ce2c565dd4023d667
PhantawatChaisri <phantawatchaisri@gmail.com>
2025-02-19 11:13:24 +0700

    Practice10

---
+++ Labsheet10/src/Barista.java
@@ -0,0 +1,25 @@
+
+public class Barista {
+	
+	private String name;
+	private char gender;
+	
+	Barista(String name,char gender){
+		this.name = name;
+		this.gender = gender;
+		
+	}
+	
+	Barista(){
+		
+	}
+	
+	public String getName () {
+		
+	}
+	
+	public String getGenderName () {
+		
+	}
+
+}
